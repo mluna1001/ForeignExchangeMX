33f8cc9b134aad9c4e6bc816a4a21d1775f7d3c6
mluna1001 <mluna1001@gmail.com>
2017-10-21 16:26:41 -0500

    Creation initial project

---
+++ ForeignExchangeMX/ForeignExchangeMXPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ForeignExchangeMX
+{
+    public partial class ForeignExchangeMXPage : ContentPage
+    {
+        public ForeignExchangeMXPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
