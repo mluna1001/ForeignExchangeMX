81f699d551dfb69e349d17ab2069f42c22580bc1
mluna1001 <mluna1001@gmail.com>
2017-10-21 19:10:19 -0500

    Agrego la consulta de varo con la API, ya calcula

--- ForeignExchangeMX/ViewModels/MainViewModel.cs
+++ ForeignExchangeMX/ViewModels/MainViewModel.cs
@@ -1,12 +1,29 @@
 ﻿namespace ForeignExchangeMX.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Net.Http;
     using System.Windows.Input;
     using ForeignExchangeMX.Models;
     using GalaSoft.MvvmLight.Command;
+    using Newtonsoft.Json;
+    using Xamarin.Forms;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        bool _isEnabled;
+        bool _isRunning;
+        string _result;
+        ObservableCollection<Rate> _rates;
+        #endregion
+
         #region Properties
         public string Amount
         {
@@ -16,8 +33,20 @@
 
         public ObservableCollection<Rate> Rates
         {
-            get;
-            set;
+            get
+            {
+                return _rates;
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
         }
 
         public Rate SourceRate
@@ -34,28 +63,103 @@
 
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
         }
 
         public bool IsEnabled
         {
-            get;
-            set;
+            get
+            {
+                return _isEnabled;
+            }
+            set
+            {
+                if (_isEnabled != value)
+                {
+                    _isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
         }
 
         public string Result
         {
-            get;
-            set;
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
         }
 
         #endregion
 
 
+        #region Constructors
         public MainViewModel()
         {
+            LoadRates();
         }
+        #endregion
+
+        #region Methods
+        async void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading rates...";
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new 
+                    Uri("http://apiexchangerates.azurewebsites.net");
+                var controller = "/api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+
+                var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+                Rates = new ObservableCollection<Rate>(rates);
+
+                IsRunning = false;
+                Result = "Ready to convert!";
+                IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message;
+            }
+        }
+        #endregion
 
         #region Commands
         public ICommand ConvertCommand
@@ -66,9 +170,55 @@
             }
         }
 
-        private void Convert()
+        async void Convert()
         {
-            
+            decimal amount = 0;
+
+            if (string.IsNullOrEmpty(Amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a value in amount",
+                    "Accept"
+                );
+                return;
+            }
+            if (!decimal.TryParse(Amount, out amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a numercic value in amount",
+                    "Accept"
+                );
+                return;
+            }
+            if (SourceRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a source rate",
+                    "Accept"
+                );
+                return;
+            }
+            if (TargetRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a source rate",
+                    "Accept"
+                );
+                return;
+            }
+
+            var amountConverted = amount / (decimal)SourceRate.TaxRate * (decimal)TargetRate.TaxRate;
+
+            Result = string.Format("{0} {1:C2} = {2} {3:C2}",
+                                   SourceRate.Code,
+                                   amount,
+                                   TargetRate.Code,
+                                   amountConverted);
+
         }
         #endregion
     }
